34e587a09dbed3255a3d7d2f216fe7fee58e5f50
TheSelfAtu <example.com>
2020-07-22 23:05:06 +0900

    first commit

--- Low-Pass-Filter.py
+++ Low-Pass-Filter.py
@@ -19,7 +19,7 @@ def printWaveInfo(wf):
 def regularization(np_array):
     np_array = np.abs(np_array)
     max = np.max(np_array)
-    print(max)
+    # print(max,'max_value')
     np_array = np_array/np.max(np_array)
     return np_array
 
@@ -41,18 +41,36 @@ def rep_bn_data(np_array, data, num):
         np_array = np.append(np_array, area*2/T0)
     return np_array
 
+# 時間遅延
+def shift_n_time(f,n):
+    new_f = f[n:]
+    return new_f
+
+# 時間遅延の信号の重ね合わせ
+def low_pass_fillter(f,n):
+    low_pass_list = np.zeros(len(f)-n)
+    for i in range(n):
+        shifted_n_data = shift_n_time(f,i)
+        low_pass_list  += shifted_n_data[:-10+i]
+    print(len(low_pass_list))
+    return low_pass_list
+
+def output_wav_file(name):
+    pass
+
+
 if __name__ == '__main__':
     wf = wave.open("r3.wav", "rb")
     buffer = wf.readframes(wf.getnframes())
-    # printWaveInfo(wf)
+    printWaveInfo(wf)
 
     N= 512
     fs = 44100
     T0 = N/fs
+    freqList = [N/T0 for N in range(int(N/2))]
+    han = np.hanning(N)    # ハニング窓e
 
-    han = np.hanning(N)    # ハニング窓
-
-     # bufferはバイナリなので2バイトずつ整数（-32768から32767）にまとめる
+    #  bufferはバイナリなので2バイトずつ整数（-32768から32767）にまとめる
     originaldata = frombuffer(buffer, dtype="int16")
     datafloat = originaldata[:N] * han
     dataint = np.array(datafloat, dtype=int)
@@ -67,13 +85,59 @@ if __name__ == '__main__':
     an_array = np.append(an_array,a0)
     an_array = rep_an_data(an_array,dataint, int(N/2))
     an_array = regularization(an_array)
-    # print(an_array)
-    # plot(an_array)
+    # plot([0]+freqList, an_array, marker='o', linestyle='-')
+    # xlabel("frequency [Hz]")
+    # ylabel("amplitude spectrum")
+
     # show()
     
     bn_array = np.array([])
     bn_array = rep_bn_data(bn_array,dataint, int(N/2))
     bn_array = regularization(bn_array)
-    print(bn_array)
-    plot(bn_array)
-    show()
+    # plot(freqList, bn_array, marker='o', linestyle='-')
+    # xlabel("frequency [Hz]")
+    # ylabel("amplitude spectrum")
+
+    # show()
+
+
+    y = low_pass_fillter(dataint,10)
+
+    an_array = np.array([])
+    a0 = y.sum()/T0
+
+    an_array = np.append(an_array,a0)
+    an_array = rep_an_data(an_array,y, int(len(y)/2))
+    an_array = regularization(an_array)
+    # plot(freqList[:-4], an_array, marker='o', linestyle='-')
+    # xlabel("frequency [Hz]")
+    # ylabel("amplitude spectrum")
+
+    # show()
+    
+    bn_array = np.array([])
+    bn_array = rep_bn_data(bn_array, y, int(len(y)/2))
+    bn_array = regularization(bn_array)
+    
+    # plot(freqList[:-5], bn_array, marker='o', linestyle='-')
+    # xlabel("frequency [Hz]")
+    # ylabel("amplitude spectrum")
+
+    # show()
+
+    
+    # fOut = wave.open("out.wav", "wb")
+    # print(y)
+    # print(len(y))
+    # fOut.setparams( (2, 2, fs, len(y), "NONE", "not compressed") )
+
+    # fOut.writeframes(y)
+
+    # fOut.close()
+    print(bytes(y))
+    w = wave.Wave_write("output.wav")
+    w.setnchannels(2)
+    w.setsampwidth(2)
+    w.setframerate(44100)
+    w.writeframes(bytes(y))
+    w.close()
